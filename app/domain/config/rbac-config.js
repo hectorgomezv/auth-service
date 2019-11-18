e23a7d49b087a9a556082a078cc48e059f2ba1fc
Hector Gomez <hector.gomez.varela@gmail.com>
2019-11-18 19:40:44 +0100

    *

--- app/domain/config/rbac-config.js
+++ app/domain/config/rbac-config.js
@@ -4,12 +4,12 @@ module.exports = {
   roles: Object.values(ROLES),
   permissions: {
     admin: ['create', 'update', 'delete'],
-    user: ['create', 'update', 'delete'],
+    user: ['create', 'update', 'delete', 'read'],
     password: ['create', 'update'],
   },
   grants: {
     [ROLES.SUPERADMIN]: ['admin', 'create_admin', 'update_admin', 'delete_admin'],
-    [ROLES.ADMIN]: ['user', 'create_user', 'update_user', 'delete_user'],
+    [ROLES.ADMIN]: ['user', 'create_user', 'update_user', 'delete_user', 'read_user'],
     [ROLES.USER]: ['create_password', 'update_password'],
   },
 };
--- app/interfaces/routers/user-router.js
+++ app/interfaces/routers/user-router.js
@@ -1,8 +1,15 @@
 const { AuthEntity } = require('../entities');
-const { createUserController } = require('../controllers/user');
+
+const {
+  getUserController,
+  getUsersController,
+  createUserController,
+} = require('../controllers/user');
 
 module.exports = (app, opts, done) => {
   app.use('/', AuthEntity.addAuthHeader);
   app.post('/', createUserController);
+  app.get('/', {}, getUsersController);
+  app.get('/:id', {}, getUserController);
   done();
 };
