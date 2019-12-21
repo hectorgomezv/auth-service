d647bd83db89c1b05818adb2c11474dd85b3957e
Hector Gomez <hector.gomez.varela@gmail.com>
2019-12-21 14:24:58 +0100

    Wrap auth in a context object

--- app/interfaces/controllers/user/create-user-controller.js
+++ app/interfaces/controllers/user/create-user-controller.js
@@ -4,9 +4,9 @@ const { createUser } = require('../../../domain/use-cases/user');
 
 module.exports = async (req, res) => {
   try {
-    const { auth } = req.raw;
+    const { context } = req.raw;
     const data = (req.body && req.body.data) || {};
-    const user = await createUser(auth, data);
+    const user = await createUser(context, data);
     res.send({ data: profileAdapter(user) });
   } catch (err) {
     throw jsonErrorAdapter(err);
